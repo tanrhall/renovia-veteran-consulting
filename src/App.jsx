a2f740fc8fa5b2edcc7963a223f6763685304638
tanrhall <info@renoviacollective.com>
2025-11-21 17:45:53 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Renovia Veteran Consulting</h1>
+      <p>Your full site code goes here. Paste the React component from ChatGPT canvas into this file.</p>
+    </div>
+  )
+}
